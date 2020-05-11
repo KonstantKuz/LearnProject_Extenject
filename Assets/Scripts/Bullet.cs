042c52716629b2a07c66648e35f142222fc461d8
Konstantin Kuznetsov <stalkercatkuz@gmail.com>
2020-05-11 23:20:27 +0300

    Added empty AudioManager

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,16 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
 
+    private AudioManager audioManager = null;
+
+    [Inject]
+    public void Construct(AudioManager manager)
+    {
+        audioManager = manager;
+    }
     private void OnTriggerEnter(Collider other)
     {
+        audioManager.PlaySound("Play hit sound!");
         GetComponent<PooledObject>().DelayedReturnToPool(0);
     }
-
+    
     void Update()
     {
         transform.position += transform.forward * moveSpeed;
--- Assets/Scripts/MyGameInstaller.cs
+++ Assets/Scripts/MyGameInstaller.cs
@@ -5,10 +5,11 @@ public class MyGameInstaller : MonoInstaller
 {
     public Player player;
     public ObjectPool pool;
-
+    public AudioManager audioManager;
     public override void InstallBindings()
     {
         Container.Bind<ObjectPool>().FromInstance(pool).AsSingle();
         Container.BindInterfacesAndSelfTo<Player>().FromInstance(player).AsSingle();
+        Container.Bind<AudioManager>().FromInstance(audioManager).AsSingle();
     }
 }
